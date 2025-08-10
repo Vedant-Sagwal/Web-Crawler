3e5c75ce20d809084ecc4a050cf50278797dd8f0
Vedant-Sagwal <vedantsagwal@gmail.com>
2025-08-10 17:00:12 +0530

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,21 +1,42 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages
+  }
+
+  const normalizedCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++
+    return pages;
+  }
+
+  pages[normalizedCurrentURL] = 1
+
+  console.log(`Actively crawling ${currentURL}`)
   try {
     const responseHTML = await fetch(currentURL);
     if (responseHTML.status > 399) {
       console.log(`Error in fetch with status code ${responseHTML.status} on page ${currentURL}`)
-      return;
+      return pages;
     }
     const contentType = responseHTML.headers.get("content-type")
     if (!contentType.includes("text/html")) {
       console.log(`Non HTMl response , content type  : ${contentType} on page ${currentURL}`);
-      return
+      return pages;
     }
-    console.log(await responseHTML.text())
+    const htmlBody = await responseHTML.text()
+    const nextURLs = getURLFromHTML(htmlBody, baseURL);
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (err) {
     console.log(`An Error occured in fetch : ${err} on page ${currentURL}`);
   }
+  return pages;
 }
 
 function getURLFromHTML(htmlBody, baseURL) {
--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const { crawlPage } = require("./crawl");
 
 
-function main() {
+async function main() {
   if (process.argv.length < 3) {
     console.log("No Website Provided!!");
     process.exit(1)
   }
   else {
     for (let i = 2; i < process.argv.length; i++) {
-      console.log(`Crawling ${process.argv[i]}`);
-      crawlPage(process.argv[i]);
+      console.log(`Starting Crawling at ${process.argv[i]}`);
+      const pages = await crawlPage(process.argv[i], process.argv[i], {});
+
+      for (const page of Object.entries(pages)) {
+        console.log(page);
+      }
     }
   }
 }
